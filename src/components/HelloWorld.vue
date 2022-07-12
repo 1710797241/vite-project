3094b3ec3c96dd761f5a3735a3165a1fbf555729
wangzhongyong <wzy@kgtdata.com>
2022-07-12 16:11:56 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+onMounted(()=>{
+  console.log('onMounted');
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
